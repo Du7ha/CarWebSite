e3707a1ac54a0044a96521624717c497f2d28c97
MohamedAshraf564 <mohamed2ashraf610@gmail.com>
2025-04-21 15:30:18 +0200

    New Cars and Used Cars

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,8 @@
 using System.Diagnostics;
-using System.Reflection.Metadata.Ecma335;
 using CarWebSite.Models;
 using Microsoft.AspNetCore.Mvc;
-namespace CarWebSite.Controllers
 
+namespace CarWebSite.Controllers
 {
     public class HomeController : Controller
     {
@@ -15,108 +14,67 @@ namespace CarWebSite.Controllers
         }
 
         public IActionResult Index()
-{
-    var photos = new List<Photo>
-    {
-        new Photo { 
-            Url = "/Images/images.png", 
-            Description = "BMW" 
-        },
-        new Photo { 
-            Url = "/Images/image.jpg", 
-            Description = "Audi" 
-        },
-        new Photo { 
-            Url = "/Images/image(1).jpg", 
-            Description = "Toyota" 
-        },
-        new Photo { 
-            Url = "/Images/Mercedes-Logo.svg.png", 
-            Description = "Mercedes" 
-        },
-        new Photo { 
-            Url = "/Images/emblem_001.jpg", 
-            Description = "Ferrari" 
+        {
+            var photos = new List<Photo>
+            {
+                new Photo { Url = "/Images/images.png", Description = "BMW" },
+                new Photo { Url = "/Images/image.jpg", Description = "Audi" },
+                new Photo { Url = "/Images/image(1).jpg", Description = "Toyota" },
+                new Photo { Url = "/Images/Mercedes-Logo.svg.png", Description = "Mercedes" },
+                new Photo { Url = "/Images/emblem_001.jpg", Description = "Ferrari" }
+            };
+
+            var categories = new List<Category>
+            {
+                new Category { Name = "sedan", Description = "Sedan" },
+                new Category { Name = "suv", Description = "SUV" },
+                new Category { Name = "truck", Description = "Truck" },
+                new Category { Name = "electric", Description = "Electric" },
+                new Category { Name = "luxury", Description = "Luxury" },
+                new Category { Name = "Coupe", Description = "Coupe" },
+                new Category { Name = "crossover", Description = "crossover" },
+                new Category { Name = "Minivan", Description = "Minivan" },
+                new Category { Name = "convertible", Description = "convertible" },
+                new Category { Name = "hatchback", Description = "hatchback" },
+                new Category { Name = "Hybrid", Description = "Hybrid" }
+            };
+
+            var viewModel = new HomeViewModel
+            {
+                Photos = photos,
+                Categories = categories
+            };
+
+            return View(viewModel);
         }
-    };
-
-    var categories = new List<Category>
-    {
-        new Category { 
-            Name = "sedan", 
-            Description = "Sedan" 
-        },
-        new Category { 
-            Name = "suv", 
-            Description = "SUV" 
-        },
-        new Category { 
-            Name = "truck", 
-            Description = "Truck" 
-        },
-        new Category { 
-            Name = "electric", 
-            Description = "Electric" 
-        },
-        new Category { 
-            Name = "luxury", 
-            Description = "Luxury" 
-        },
-        new Category { 
-            Name = "Coupe", 
-            Description = "Coupe" 
-        },
-        new Category { 
-            Name = "crossover", 
-            Description = "crossover" 
-        },
-        new Category { 
-            Name = "Minivan", 
-            Description = "Minivan" 
-        },
-        new Category { 
-            Name = "convertible", 
-            Description = "convertible" 
-        },
-        new Category { 
-            Name = "hatchback", 
-            Description = "hatchback" 
-        },
-        new Category { 
-            Name = "Hybrid", 
-            Description = "Hybrid" 
-        }
-    };
-
-    // Combine both lists into a ViewModel
-    var viewModel = new HomeViewModel
-    {
-        Photos = photos,
-        Categories = categories
-    };
-
-    // Pass the ViewModel to the view
-    return View(viewModel);
-}
 
         public IActionResult Privacy()
         {
             return View();
         }
-         public IActionResult NewCars()
+
+        public IActionResult NewCars()
         {
+            ViewBag.BodyTypes = Enum.GetNames(typeof(BodyType));
+            ViewBag.Brands = new List<string> { "Ford", "Toyota", "Honda", "BMW", "Mercedes", "Audi", "Chevrolet", "Nissan", "Porsche", "Rolls-Royce", "Mahindra" };
+            ViewBag.Colors = new List<string> { "Black", "White", "Silver", "Red", "Blue", "Green", "Yellow", "Gray" };
             return View();
         }
 
         public IActionResult UsedCars()
         {
+            ViewBag.BodyTypes = Enum.GetNames(typeof(BodyType));
+            ViewBag.Brands = new List<string> { "Ford", "Toyota", "Honda", "BMW", "Mercedes", "Audi", "Chevrolet", "Nissan", "Porsche", "Rolls-Royce", "Mahindra" };
+            ViewBag.Colors = new List<string> { "Black", "White", "Silver", "Red", "Blue", "Green", "Yellow", "Gray" };
             return View();
         }
+
         public IActionResult OfferYourCar()
         {
             return View();
         }
-         public IActionResult Profile()
+
+        public IActionResult Profile()
         {
             return View();
         }
@@ -125,11 +83,16 @@ namespace CarWebSite.Controllers
             return View();
         }
 
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult ToggleFavorite(int carId)
+        {
+            return Json(new { success = true, message = "Toggled favorite status" });
         }
     }
 }
