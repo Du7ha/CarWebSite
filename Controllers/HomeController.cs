11add1276e9be870f595a0497594ee2cfb343d52
mo2menaref <mo2menaref34@yahoo.com>
2025-04-21 02:45:36 +0200

    Design login frame and Menu list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -120,6 +120,10 @@ namespace CarWebSite.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
